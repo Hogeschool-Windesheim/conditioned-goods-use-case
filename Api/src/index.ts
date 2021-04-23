1011b9c5330fa72497ac9fe7b6eb643f9df071df
Berat Guzel <s1127994@student.windesheim.nl>
2021-04-23 15:30:23 +0200

    combined the server.ts and index.ts

--- Api/src/index.ts
+++ Api/src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+
+// require('dotenv').config();
+const port = process.env.PORT;
 
 const app = express();
 
+// define route for the default home page
 app.get('/', (req, res) => {
     res.send("Hello World, from hyperledgerfabric Conditionedgoods!");
 });
+
+// start the Express server
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`App is running on http://localhost:${port}`)
+  });
 
 export default app;
