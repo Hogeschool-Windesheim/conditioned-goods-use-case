ed70d756d83ee32034615c64fa9fe37d1662ce42
Berat Guzel <s1127994@student.windesheim.nl>
2021-04-30 14:51:16 +0200

    cleanup before merge

--- Api/src/controllers/shipments.ts
+++ Api/src/controllers/shipments.ts
@@ -1,5 +1,4 @@
 import {Request, Response} from "express";
-import { isConstructorDeclaration } from "typescript";
 import {connect} from '../gateway';
 import {toObject} from '../helpers';
 import {Shipment} from '../types';
