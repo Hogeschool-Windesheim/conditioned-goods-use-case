826706a7398b5909eff8550a746f807474b31c70
Sjoerd <kerkhof.sjoerd@gmail.com>
2021-05-18 15:12:07 +0200

    Delete Hyperledger directory

--- Hyperledger/chaincode/src/libs/validate.ts
+++
@@ -1,7 +0,0 @@
-/** 
- * Validate if an value is in range of a min and a max.
- * (We use the absolute value to calculate this for negative values)
- */
-export function isInRange(value: number, min: number, max: number) {
-    return Math.abs(value) > Math.abs(min) && Math.abs(value) < Math.abs(max); 
-}
