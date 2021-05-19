2c14d3c6bb5559dd51947801516d17a874ea12a4
Berat Guzel <s1127994@student.windesheim.nl>
2021-05-19 15:12:07 +0200

    all schipments finished

--- frontend/src/views/Index.js
+++ frontend/src/views/Index.js
@@ -32,26 +32,26 @@ export default function Index() {
       <section className="container header pt-20 flex h-screen max-h-860-px">
         <div className="container">
         <div className="block text-3xl mb-2">All shipments: </div>
-          <table className="table-fixed border-2 border-black w-max">
+          <table className="border-2 border-black w-full text-left ">
             <thead>
               <tr>
-                <th className="border-2 border-black w-1/7">Id</th>
-                <th className="border-2 border-black w-1/7">Sensors</th>
-                <th className="border-2 border-black w-1/7">Current sensor</th>
-                <th className="border-2 border-black w-1/7">Temperature</th>
-                <th className="border-2 border-black w-1/7">Timestamp</th>
-                <th className="border-2 border-black w-1/7">more</th>
+                <th className="border-2 border-black">Id</th>
+                <th className="border-2 border-black">Sensors</th>
+                <th className="border-2 border-black">Current sensor</th>
+                <th className="border-2 border-black">Temperature</th>
+                <th className="border-2 border-black">Timestamp</th>
+                <th className="border-2 border-black">more</th>
               </tr>
             </thead>
             <tbody>
             {data.map(({id, temperature, sensors}) => 
               <tr key={id}>
-                <td>{id}</td> 
-                <td>{listViewOrdener(sensors)}</td>
-                <td>{temperature.sensorID}</td>
-                <td>{temperature.value}°C</td>
-                <td>{timeConverter(temperature.timestamp)} </td>
-                <td><Link to="/views/singelShipment"><button>More info</button></Link></td>
+                <td className="border-2 border-black">{id}</td> 
+                <td className="border-2 border-black">{listViewOrdener(sensors)}</td>
+                <td className="border-2 border-black">{temperature.sensorID}</td>
+                <td className="border-2 border-black">{temperature.value}°C</td>
+                <td className="border-2 border-black">{timeConverter(temperature.timestamp)} </td>
+                <td className="border-2 border-black"><a href={"/singelShipment/" + id}>More Info</a></td>
               </tr>
             )}
             </tbody>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,91 +14,28 @@ module.exports = {
       "./public/**/*.js",
       "./public/*.js",
     ],
-    options: {
-      safelist: [],
-    },
   },
+
   theme: {
     colors: {
-      ...colors,
+      black: colors.black,
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
     },
+  },
+  
+  variants: {
     extend: {
-      minHeight: {
-        "screen-75": "75vh",
-      },
-      fontSize: {
-        55: "55rem",
-      },
-      opacity: {
-        80: ".8",
-      },
-      zIndex: {
-        2: 2,
-        3: 3,
-      },
-      inset: {
-        "-100": "-100%",
-        "-225-px": "-225px",
-        "-160-px": "-160px",
-        "-150-px": "-150px",
-        "-94-px": "-94px",
-        "-50-px": "-50px",
-        "-29-px": "-29px",
-        "-20-px": "-20px",
-        "25-px": "25px",
-        "40-px": "40px",
-        "95-px": "95px",
-        "145-px": "145px",
-        "195-px": "195px",
-        "210-px": "210px",
-        "260-px": "260px",
-      },
-      height: {
-        "95-px": "95px",
-        "70-px": "70px",
-        "350-px": "350px",
-        "500-px": "500px",
-        "600-px": "600px",
-      },
-      maxHeight: {
-        "860-px": "860px",
-      },
-      maxWidth: {
-        "100-px": "100px",
-        "120-px": "120px",
-        "150-px": "150px",
-        "180-px": "180px",
-        "200-px": "200px",
-        "210-px": "210px",
-        "580-px": "580px",
-      },
-      minWidth: {
-        "140-px": "140px",
-        48: "12rem",
-      },
-      backgroundSize: {
-        full: "100%",
-      },
+      tableLayout: ['hover', 'focus'],
+      width: ['hover', 'focus'],
+      borderColor: ['active'],
+      opacity: ['disabled'],
+      borderWidth: ['hover', 'focus'],
     },
   },
-  variants: [
-    "responsive",
-    'hover',
-    "group-hover",
-    "focus-within",
-    "first",
-    "last",
-    "odd",
-    "even",
-    "focus",
-    "active",
-    "visited",
-    "disabled",
-  ],
-  extend:{
-    tableLayout: ['hover', 'focus'],
-    width: ['hover', 'focus'],
-  },
+
   plugins: [
     require("@tailwindcss/forms"),
     plugin(function ({ addComponents, theme }) {
@@ -107,6 +44,7 @@ module.exports = {
         {
           ".container": { width: "100%" },
         },
+
         {
           [`@media (min-width: ${screens.sm})`]: {
             ".container": {
@@ -114,6 +52,7 @@ module.exports = {
             },
           },
         },
+
         {
           [`@media (min-width: ${screens.md})`]: {
             ".container": {
@@ -121,6 +60,7 @@ module.exports = {
             },
           },
         },
+
         {
           [`@media (min-width: ${screens.lg})`]: {
             ".container": {
@@ -128,6 +68,7 @@ module.exports = {
             },
           },
         },
+
         {
           [`@media (min-width: ${screens.xl})`]: {
             ".container": {
@@ -135,6 +76,7 @@ module.exports = {
             },
           },
         },
+
         {
           [`@media (min-width: ${screens["2xl"]})`]: {
             ".container": {
