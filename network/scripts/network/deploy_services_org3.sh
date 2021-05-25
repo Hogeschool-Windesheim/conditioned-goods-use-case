4b26271459487f4e55a74ec87ef2df554682046b
ThijsMansveld <44668998+ThijsMansveld@users.noreply.github.com>
2021-05-25 17:26:55 +0200

    Update deploy_services_org3.sh

--- network/scripts/network/deploy_services_org3.sh
+++ network/scripts/network/deploy_services_org3.sh
@@ -6,8 +6,8 @@ set -ev
 
 # ORG 3
 docker stack deploy -c "$ORDERER2_COMPOSE_PATH" hlf_orderer
-sleep 3
+sleep 9
 docker stack deploy -c "$SERVICE_ORG3_COMPOSE_PATH" hlf_services
-sleep 3
+sleep 9
 docker stack deploy -c "$PEER_ORG3_COMPOSE_PATH" hlf_peer
 
