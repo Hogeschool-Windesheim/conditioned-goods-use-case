348d2bb9435925c3f9bc68086e391f6bd50c95a7
ThijsMansveld <44668998+ThijsMansveld@users.noreply.github.com>
2021-05-27 01:51:33 +0200

    Update start_all.sh

--- network/start_all.sh
+++ network/start_all.sh
@@ -1,9 +1,6 @@
 ./scripts/network/deploy_services_kafka.sh
-sleep 15
 ./scripts/network/deploy_services_org1.sh
-sleep 3
 ./scripts/network/deploy_services_org2.sh
-sleep 3
 ./scripts/network/deploy_services_org3.sh
 
 
