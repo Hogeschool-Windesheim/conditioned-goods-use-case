80746f38c98f0e4b09dd9c5de651819e3b399b45
ThijsMansveld <44668998+ThijsMansveld@users.noreply.github.com>
2021-05-27 02:21:57 +0200

    Update deploy_services_org1.sh

--- network/scripts/network/deploy_services_org1.sh
+++ network/scripts/network/deploy_services_org1.sh
@@ -7,8 +7,7 @@ set -ev
 # ORG 1
 
 docker stack deploy -c "$ORDERER0_COMPOSE_PATH" hlf_orderer
-sleep 9
+sleep 3
 docker stack deploy -c "$SERVICE_ORG1_COMPOSE_PATH" hlf_services
-sleep 9
+sleep 3
 docker stack deploy -c "$PEER_ORG1_COMPOSE_PATH" hlf_peer
-sleep 9
