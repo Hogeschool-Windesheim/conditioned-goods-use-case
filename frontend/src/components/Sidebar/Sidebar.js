436223ff1b9d9c12eb2118f3b2291bcecd08fc65
gwesseling <gerardwesseling16@gmail.com>
2021-05-27 15:29:36 +0200

    refactor: cleanup

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -130,7 +130,7 @@ export default function Sidebar() {
                         : "text-blueGray-300")
                     }
                   ></i>{" "}
-                  Shipments
+                  All shipments
                 </Link>
               </li>
             </ul>
--- frontend/src/layouts/Admin.js
+++ frontend/src/layouts/Admin.js
@@ -10,23 +10,16 @@ import FooterAdmin from "components/Footers/FooterAdmin.js";
 import PaginationProvider from "components/Providers/Pagination.js";
 
 // views
-<<<<<<< Updated upstream
 import Dashboard from "views/admin/Dashboard/index.js";
 import Shipment from "views/admin/Shipment/index.js";
-import AddShipment from "views/admin/AddShipment.js";
-import Shipments from "views/admin/Shipments/index.js";
-=======
-import Dashboard from "views/admin/Dashboard.js";
-// import Shipment from "views/admin/Shipment/index.js";
 import AddShipment from "views/admin/AddShipment/index.js";
->>>>>>> Stashed changes
+import Shipments from "views/admin/Shipments/index.js";
 
 export default function Admin() {
   return (
     <>
     <SnackbarProvider>
       <Sidebar />
-<<<<<<< Updated upstream
       <PaginationProvider className="h-full overflow-y-auto relative md:ml-64 bg-blueGray-100">
           <AdminNavbar />
           {/* Header */}
@@ -34,31 +27,15 @@ export default function Admin() {
           <div className="px-4 md:px-10 mx-auto min-h-full w-full -m-24">
             <Switch>
               <Route path="/admin/dashboard" exact component={Dashboard} />
+              <Route path="/admin/shipment/add" exact component={AddShipment} />
               <Route path="/admin/shipment/:id" exact component={Shipment} />
               <Route path="/admin/shipments" exact component={Shipments} />
-              <Route path="/admin/shipment/add" exact component={AddShipment} />
               <Redirect from="/admin" to="/admin/dashboard" />
             </Switch>
             <FooterAdmin />
           </div>
       </PaginationProvider>
-=======
-      <div className="min-h-screen relative md:ml-64 bg-blueGray-100">
-        <AdminNavbar />
-        {/* Header */}
-        <HeaderStats />
-        <div className="px-4 md:px-10 mx-auto min-h-full w-full -m-24">
-          <Switch>
-            <Route path="/admin/dashboard" exact component={Dashboard} />
-            <Route path="/admin/shipment/add" exact component={AddShipment} />
-            {/* <Route path="/admin/shipment/:id" exact component={Shipment} /> */}
-            <Redirect from="/admin" to="/admin/dashboard" />
-          </Switch>
-          <FooterAdmin />
-        </div>
-      </div>
-    </SnackbarProvider>
->>>>>>> Stashed changes
+      </SnackbarProvider>
     </>
   );
 }
