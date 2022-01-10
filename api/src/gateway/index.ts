fa7fb9c0414452dbc12018106b40059ed9205be4
shadawo <ribe.thibaut@gmail.com>
2022-01-10 05:50:47 -0800

    Temporarily added functions to ensure user creation

--- api/src/gateway/index.ts
+++ api/src/gateway/index.ts
@@ -1,7 +1,7 @@
 import { Gateway, GatewayOptions, Wallets } from "fabric-network";
 import * as path from "path";
 // TODO: make import of connection profile by path instead of import
-import org1 from "../profiles/connection-org1.json";
+
 import { buildCCPOrg1, buildWallet } from "../utils/AppUtil";
 import {
     buildCAClient,
@@ -16,9 +16,28 @@ const org1UserId = "appUser";
  * Setup a gateway conenction to the HLF Network.
  */
 export async function connect(): Promise<Gateway> {
+    // build an in memory object with the network configuration (also known as a connection profile)
     const ccp = buildCCPOrg1();
-    const wallet = await Wallets.newFileSystemWallet(walletPath);
 
+    // build an instance of the fabric ca services client based on
+    // the information in the network configuration
+    const caClient = buildCAClient(ccp, "ca.org1.example.com");
+
+    // setup the wallet to hold the credentials of the application user
+    const wallet = await buildWallet(walletPath);
+
+    // // in a real application this would be done on an administrative flow, and only once
+    await enrollAdmin(caClient, wallet, mspOrg1);
+
+    // in a real application this would be done only when a new user was required to be added
+    // and would be part of an administrative flow
+    await registerAndEnrollUser(
+        caClient,
+        wallet,
+        mspOrg1,
+        org1UserId,
+        "org1.department1"
+    );
     // Create a new gateway instance for interacting with the fabric network.
     // In a real application this would be done as the backend server session is setup for
     // a user that has been verified.
